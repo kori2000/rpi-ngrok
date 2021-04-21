3c4bfa8d8d80635e2c1cd716daff4fa82d681547
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2021-04-22 01:49:26 +0200

    :fire: first init

--- run.sh
+++ run.sh
@@ -13,4 +13,6 @@ if [[ "$(docker images -q rpi-ngrok:latest 2> /dev/null)" == "" ]]; then
 fi 
 
 echo " ---> Starting ngrok"
-docker run --rm -it rpi-ngrok:latest ngrok http 192.168.1.110:3000
+docker run --rm -it rpi-ngrok:latest ngrok http 192.168.1.110:3000 -d
+
+docker ps -a
