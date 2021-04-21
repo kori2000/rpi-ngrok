5e72e9aaca4e7ccc9e83ae2a115409fb3cc98cd2
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2021-04-22 01:35:54 +0200

    :fire: first init

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ clear
 echo "Starting ngrok..."
 
 # Build Docker Image of BC Gov Indy Explorer
-if [[ "$(docker images -q rpi-ngrok 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q rpi-ngrok:latest 2> /dev/null)" == "" ]]; then
     docker build --tag rpi-ngrok .
 fi 
 
-docker run --rm -it rpi-ngrok ngrok http localhost:3000
+docker run --rm -it rpi-ngrok:latest ngrok http localhost:3000
