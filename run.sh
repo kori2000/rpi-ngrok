507ffdb74f52a43a49eb27f721079986f7ba0445
Koraltan Kaynak <koraltan.kaynak@deutschebahn.com>
2021-04-22 02:39:08 +0200

    :fire: first init

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Filter JSON Value
-function jsonValue() {
-    KEY=$1
-    num=$2
-    awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
-}
-
 clear
 echo ""
 echo " --- RPI NGROK --- "
@@ -22,4 +15,7 @@ fi
 echo " ---> Starting ngrok"
 docker run -d --name rpi-ng --rm -it rpi-ngrok:latest ngrok http 192.168.1.110:3000
 
-docker logs rpi-ng | jsonValue id
+docker logs rpi-ng
+
+echo " "
+echo " # Stop Tunneling with: docker stop rpi-ng"
